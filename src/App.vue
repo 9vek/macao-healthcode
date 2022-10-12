f3480f983e25f2b9b7f0ce013846943362cff073
Kevyn <532775134@qq.com>
2022-10-12 13:55:54 +0800

    style fix

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="container max-w-md min-h-fit bg-gray-100 my-16 pb-4 rounded-md overflow-hidden shadow-lg mx-auto">
+  <div class="container max-w-md min-h-fit bg-gray-100 sm:my-16 pb-4 rounded-md overflow-hidden shadow-lg mx-auto p-1">
     <router-view></router-view>
   </div>
 </template>
